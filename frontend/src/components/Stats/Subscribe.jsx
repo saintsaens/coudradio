f987611630d8b81b4e23a499fbd929062627aa66
Flavien <saintsaens@users.noreply.github.com>
2025-03-06 12:49:01 +0100

    Hide Stripe link and append userId

--- frontend/src/components/Stats/Subscribe.jsx
+++ frontend/src/components/Stats/Subscribe.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { useEffect } from "react";
 import { Typography } from "@mui/material";
+import { useSelector } from "react-redux";
 
-const baseUrl = import.meta.env.VITE_BACKEND_URL;
-
-const handleSubscribe = () => {
-    console.log("Flying off to Stripe!");
-    const paymentLink = "https://buy.stripe.com/test_aEUdRm0qIafx3LObII";
-
-    window.location.href = paymentLink; // Redirect user to Google
-};
+const paymentBaseUrl = import.meta.env.VITE_PAYMENT_BASE_URL;
 
 const Subscribe = () => {
+    const { userId } = useSelector((state) => state.user);
+    
+    const handleSubscribe = () => {
+        const paymentLink = `${paymentBaseUrl}?userId=${userId}`;
+        window.location.href = paymentLink;
+    };
+
     useEffect(() => {
         const handleKeyDown = (event) => {
             if (!(event.metaKey || event.ctrlKey) && event.key.toLowerCase() === 's') {
