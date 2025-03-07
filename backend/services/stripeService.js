978efedb09a3096ff511395c1d305381909ba9a4
Flavien <saintsaens@users.noreply.github.com>
2025-03-07 09:35:11 +0100

    Update session data upon successful payment checkout

--- backend/services/stripeService.js
+++ backend/services/stripeService.js
@@ -2,6 +2,11 @@ import * as usersRepository from "../repositories/usersRepository.js"
 
 export const handleSuccessfulSessionCheckout = async (userId) => {
     const subscribed = true;
+
+    // Update user’s subscribed status in the session
+    req.user.subscribed = subscribed;
+
+    // Update user's subscribed status in the database (persist the change)
     const result = await usersRepository.updateUser(userId, { subscribed });
 
     return result;
