06fcda0c6fb988351c25eaabb59dab26d37bac6d
Flavien <saintsaens@users.noreply.github.com>
2025-03-07 10:13:56 +0100

    Remove mention of req in stripe’s webhook handler

--- backend/services/stripeService.js
+++ backend/services/stripeService.js
@@ -3,9 +3,6 @@ import * as usersRepository from "../repositories/usersRepository.js"
 export const handleSuccessfulSessionCheckout = async (userId) => {
     const subscribed = true;
 
-    // Update user’s subscribed status in the session
-    req.user.subscribed = subscribed;
-
     // Update user's subscribed status in the database (persist the change)
     const result = await usersRepository.updateUser(userId, { subscribed });
 
