67f6ade0c87e6684e867183bce73ac3d8231775e
Flavien <saintsaens@users.noreply.github.com>
2025-03-24 21:02:53 +0100

    Cosmetics

--- backend/services/trackEncodingService.js
+++ backend/services/trackEncodingService.js
@@ -8,28 +8,28 @@ const unlink = util.promisify(fs.unlink);
 export const encodeTrack = (index, playlist, channelPath) => {
     const currentTrack = playlist[index];
     const playlistPath = `${channelPath}/track${index}.mpd`;  // Output MPD file path
-    
+
     return new Promise((resolve, reject) => {
         const command = ffmpeg()
-        .input(currentTrack)
-        .output(playlistPath)
-        .audioCodec('aac')
-        .format('dash')
-        .outputOptions([
-            '-dash_segment_type', 'mp4',     // Use MP4 segments
-            '-use_timeline', '0',             // Disable timeline (use sequence numbers)
-            '-use_template', '1',             // Enable templates for segment names
-            '-init_seg_name', `track${index}_init.mp4`,  // Name for the initialization segment
-            '-media_seg_name', `track${index}_$Number$.m4s`,  // Template for media segments
-        ])
-        .on('error', (err) => {
-            console.error('FFmpeg error:', err.message);
-            reject(err);
-        })
-        .on('end', () => {
-            resolve(playlistPath);
-        });
-        
+            .input(currentTrack)
+            .output(playlistPath)
+            .audioCodec('aac')
+            .format('dash')
+            .outputOptions([
+                '-dash_segment_type', 'mp4',     // Use MP4 segments
+                '-use_timeline', '0',             // Disable timeline (use sequence numbers)
+                '-use_template', '1',             // Enable templates for segment names
+                '-init_seg_name', `track${index}_init.mp4`,  // Name for the initialization segment
+                '-media_seg_name', `track${index}_$Number$.m4s`,  // Template for media segments
+            ])
+            .on('error', (err) => {
+                console.error('FFmpeg error:', err.message);
+                reject(err);
+            })
+            .on('end', () => {
+                resolve(playlistPath);
+            });
+
         command.run();
     });
 };
@@ -40,7 +40,7 @@ export const encodeTracks = async (playlist, channel) => {
         fs.mkdirSync(channelPath, { recursive: true });
     }
     const mpdPaths = [];
-    
+
     for (let index = 0; index < playlist.length; index++) {
         // Encode the track
         console.log(`Encoding track${index}…`);
@@ -50,6 +50,7 @@ export const encodeTracks = async (playlist, channel) => {
         // Upload the track segments
         // console.log(`Uploading segments for track${index}`);
         // await uploadTrackSegments(mpdPath);
+
         // // Delete the local segment files
         // const directory = path.dirname(mpdPath);
         // const baseName = path.basename(mpdPath, '.mpd');
