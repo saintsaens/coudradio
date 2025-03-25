9c48e466cc249babecb64a4e95fcfd33a2b45728
Flavien <saintsaens@users.noreply.github.com>
2025-03-25 11:51:39 +0100

    Navigate dynamically to the channel

--- frontend/src/components/AppRoutes.jsx
+++ frontend/src/components/AppRoutes.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate, useParams } from 'react-router-dom';
 import Channel from "./Channel";
+
+function ChannelWrapper() {
+    const { channelName } = useParams();
+    return <Channel channelName={channelName} />;
+}
 
 function AppRoutes() {
     return (
         <Routes>
             <Route path="/" element={<Navigate to="/lofi" replace />} />
-            <Route path="/lofi" element={<Channel channelName={`lofi`}/>} />
-            <Route path="/coudrier" element={<Channel channelName={`coudrier`} />} />
+            <Route path="/:channelName" element={<ChannelWrapper />} />
         </Routes>
     );
-};
+}
 
 export default AppRoutes;
