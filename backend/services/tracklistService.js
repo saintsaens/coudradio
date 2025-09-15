bdc75586ba662db86a5076496bb78aa3eb84d0e3
Flavien <saintsaens@users.noreply.github.com>
2025-09-15 10:39:20 +0200

    Add error

--- backend/services/tracklistService.js
+++ backend/services/tracklistService.js
@@ -6,6 +6,9 @@ dotenv.config();
 export const getTracklist = async (channel) => {
     console.log(`Fetching tracks from ${channel}…`);
     const tracks = await getTracks(channel);
+    if (tracks.length === 0) {
+        throw new Error(`No tracks found for channel: ${channel}`);
+    }
     const trackUrls = await Promise.all(
         tracks.map(track => getTrackUrl(channel, track))
     );
