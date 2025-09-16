79070a22b4ae3b9dda1c2d773c7fb26665c1f696
Flavien <saintsaens@users.noreply.github.com>
2025-09-16 14:54:12 +0200

    Add mobile apps

---
+++ mobile/constants.ts
@@ -0,0 +1,12 @@
+// shared/constants.ts
+export const CHANNELS: string[] = [
+  "lofi",
+  "coudrier",
+  "japanese-rnb",
+  "alan-watts",
+  "pretty-lights",
+  "hotel-costes",
+  "macroblank",
+  "shpongle",
+  "kaamelott",
+];
