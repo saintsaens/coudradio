67ebb2385dec688ba5a7b24c7946707362a45b2d
saintsaens <saintsaens@users.noreply.github.com>
2026-03-29 14:06:12 +0200

    feat: show listening time stats to all logged-in users

--- frontend/src/components/Stats/StatsBody.jsx
+++ frontend/src/components/Stats/StatsBody.jsx
@@ -4,17 +4,13 @@ import { useSelector } from "react-redux";
 import ListeningTime from "./ListeningTime";
 
 const StatsBody = () => {
-    const { username, isSubscriber, email } = useSelector((state) => state.user);
+    const { username, email } = useSelector((state) => state.user);
 
     return (
         <Box sx={{ width: 360 }}>
             {!username ? (
                 <Typography variant="body2" sx={{ opacity: 0.4 }}>
                     log in to track your time.
-                </Typography>
-            ) : !isSubscriber ? (
-                <Typography variant="body2" sx={{ opacity: 0.4 }}>
-                    subscribe for 5€/month to see your listen time.
                 </Typography>
             ) : (
                 <ListeningTime />
